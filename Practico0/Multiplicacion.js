0332e976779e626ca2804911295785686b567383
Carlos8711 <112145939+Carlos8711@users.noreply.github.com>
2023-06-12 03:31:02 -0300

    Add files via upload
    Agregado de Resta y Multiplicacion

---
+++ Practico0/Multiplicacion.js
@@ -0,0 +1,7 @@
+let var1=35, var2=2, var3=4, resultado;
+
+
+resultado=var1*var2*var3;
+console.log(resultado);
+resultado=2*17*6;
+console.log(resultado);
---
+++ Practico0/Resta.js
@@ -0,0 +1,6 @@
+let minuendo=25;
+let sustraendo= 22;
+resta=minuendo-sustraendo;
+console.log(resta);
+resta=10-2;
+console.log(resta);
