286a19a0d713a53823f3d3a187bfacf6279c37f1
Carlos8711 <112145939+Carlos8711@users.noreply.github.com>
2023-06-19 13:34:11 -0300

    Delete Practico2 directory

--- Practico2/positivoNegativo.js
+++
@@ -1,8 +0,0 @@
-const readlineSync = require('readline-sync');
-const numero = readlineSync.question("Ingresa un numero\n");
-if(numero%2==0){
-    console.log("Es Positivo")
-}else
-{
-    console.log("Es Negativo")
-}
